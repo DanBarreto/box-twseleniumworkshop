499e875302f51c88376ba98a84d644765a141e43
Daniel Barreto <dbarreto@thoughtworks.com>
2015-10-14 17:11:56 -0300

    Add script to provision project dependencies

---
+++ provision_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# 1. Update packages
+sudo apt-get update
+
+# 2. Install java
+sudo apt-get install -y openjdk-7-jdk
+echo 'export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64' >> ~/.bashrc
+source ~/.bashrc
+
+# 3. Download IDE
+wget -O intellij.tar.gz https://download.jetbrains.com/idea/ideaIC-14.1.5.tar.gz
+tar xfz intellij.tar.gz
+
+# 4. Install git
+sudo apt-get install -y git
+
+# 5. Setup workshop project
+git clone https://github.com/ThoughtWorksInc/selenium_workshop_skeleton.git
+./selenium_workshop_skeleton/gradlew
