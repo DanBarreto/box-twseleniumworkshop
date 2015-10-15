ef2c44f804ad8b1a1b781036166e1e13aef5160a
Daniel Barreto <dbarreto@thoughtworks.com>
2015-10-15 15:59:18 -0300

    Add link helpers

--- provision_script.sh
+++ provision_script.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 set -e
 
-# 1. Update packages
+echo '*********************'
+echo '*  UPDATE PACKAGES  *'
+echo '*********************'
 sudo apt-get update
 
-# 2. Install java
+echo '******************'
+echo '*  INSTALL JAVA  *'
+echo '******************'
 sudo apt-get install -y openjdk-7-jdk
-echo 'export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64' >> ~/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> ~/.bashrc
 source ~/.bashrc
 
-# 3. Download IDE
+echo '************************'
+echo '*  CONFIGURE INTELLIJ  *'
+echo '************************'
 wget -O intellij.tar.gz https://download.jetbrains.com/idea/ideaIC-14.1.5.tar.gz
 tar xfz intellij.tar.gz
+sudo ln -s ~/idea-*/bin/idea.sh /usr/local/bin/idea
 
-# 4. Install git and gradle
+echo '****************************'
+echo '*  INSTALL GIT AND GRADLE  *'
+echo '****************************'
 sudo apt-get install -y git gradle
 
-# 5. Setup workshop project
+echo '*************************************'
+echo '*  SETUP SELENIUM WORKSHOP PROJECT  *'
+echo '*************************************'
 git clone https://github.com/ThoughtWorksInc/selenium_workshop_skeleton.git
+echo "alias selenium='cd ~/selenium_workshop_skeleton'" >> ~/.bashrc
+source ~/.bashrc
